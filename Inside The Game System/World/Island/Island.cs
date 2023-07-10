f0a62b8cd17e922646c41dccca05e3efa982c0a3
mak7on22 <mur-mur-0998@mail.ru>
2023-07-11 02:13:46 +0600

    Aded class Island

---
+++ Inside The Game System/World/Island/Island.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGame.Inside_The_Game_System.World.Island
+{
+    /// <summary>
+    /// Остров
+    /// </summary>
+    public class Island : InsideTheGameSystem
+    {
+        public string? NameIsland { get; set; }
+        public float? LvlIsland { get; set; }
+    }
+}
