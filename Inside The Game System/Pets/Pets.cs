b9d1ce27464b744b6c203119150fa28c76606bb3
mak7on22 <mur-mur-0998@mail.ru>
2023-07-11 02:14:48 +0600

    Aded class Pets

---
+++ Inside The Game System/Pets/Pets.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGame.Inside_The_Game_System.Pets
+{
+    /// <summary>
+    /// Питомцы
+    /// </summary>
+    public class Pets : InsideTheGameSystem
+    {
+        /// <summary>
+        /// ИМЯ ПИТОМЦА
+        /// </summary>
+        public string? NamePets { get; set; }
+        /// <summary>
+        /// ЛВЛ ПИТОМЦА
+        /// </summary>
+        public float LvlPets { get; set; }
+        /// <summary>
+        /// РАСА ПИТОМЦА
+        /// </summary>
+        public string? SpeciesPets { get; set; }
+        /// <summary>
+        /// РЕДКОСТЬ ПИТОМЦА
+        /// </summary>
+        public string? RarePets { get; set; }
+
+        public void SetRandomRarity()
+        {
+            Random random = new();
+            int randomNumber = random.Next(100); 
+            if (randomNumber < 50)
+                RarePets = "обычный";
+            else if (randomNumber < 90)
+                RarePets = "эпический";
+            else
+                RarePets = "легендарный";
+        }
+    }
+}
