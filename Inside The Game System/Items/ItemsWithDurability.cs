5442c7a5fdb42155af62a3fdae8b43e66b8ef499
mak7on22 <mur-mur-0998@mail.ru>
2023-07-11 02:17:37 +0600

    Aded class ItemsWithDurability

---
+++ Inside The Game System/Items/ItemsWithDurability.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGame.Inside_The_Game_System.Items
+{
+    /// <summary>
+    /// Предметы С прочностью
+    /// </summary>
+    public class ItemsWithDurability : Items 
+    {
+        /// <summary>
+        /// Прочность предмета
+        /// </summary>
+        public float Endurance { get; set; }
+
+    }
+}
