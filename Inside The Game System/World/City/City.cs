287033502606eabd13dd8f99e8177ca9a7a15e80
mak7on22 <mur-mur-0998@mail.ru>
2023-07-11 02:14:28 +0600

    Aded class City

---
+++ Inside The Game System/World/City/City.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGame.Inside_The_Game_System.World.City
+{
+    /// <summary>
+    /// ГОРОДА
+    /// </summary>
+    public class City : InsideTheGameSystem
+    {
+        /// <summary>
+        /// Название города
+        /// </summary>
+        public string? Name { get; set; }
+        /// <summary>
+        /// Lvl города
+        /// </summary>
+        public byte? LvlCity { get; set; }
+    }
+}
