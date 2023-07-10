5cffa627dbe40f4d46bed03919b785b847e93b72
mak7on22 <mur-mur-0998@mail.ru>
2023-07-11 02:16:32 +0600

    Aded class Movement

---
+++ Inside The Game System/Movement/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGame.Inside_The_Game_System.Movement
+{
+    /// <summary>
+    /// Передвижение 
+    /// </summary>
+    public class Movement
+    {
+        /// <summary>
+        /// НАЗВАНИЕ ОБЪЕКТА ДЛЯ ПЕРЕДВИЖЕНИЯ
+        /// </summary>
+        public string? NameMovement {  get; set; }
+        /// <summary>
+        /// Лвл ОБЪЕКТА ДЛЯ ПЕРЕДВИЖЕНИЯ
+        /// </summary>
+        public float LvlMovement { get; set; }
+    }
+}
