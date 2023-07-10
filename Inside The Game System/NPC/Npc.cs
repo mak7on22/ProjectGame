2e0d49b6f38fb123dc1a13710bf2cb21506ddc8c
mak7on22 <mur-mur-0998@mail.ru>
2023-07-11 02:15:13 +0600

    Aded class Npc

---
+++ Inside The Game System/NPC/Npc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGame.Inside_The_Game_System.NPC
+{
+    /// <summary>
+    /// БОТ
+    /// </summary>
+    public class Npc : InsideTheGameSystem
+    {
+        /// <summary>
+        /// Имя  Бота
+        /// </summary>
+        public string? NameNpc { get; set; }
+        /// <summary>
+        /// Lvl  Бота
+        /// </summary>
+        public float LvlNpc { get; set; }
+    }
+}
