f9bf14dbb667679f26e13be657d9b3459f96005a
mak7on22 <mur-mur-0998@mail.ru>
2023-07-11 02:16:16 +0600

    Aded class HostileNpc

---
+++ Inside The Game System/NPC/HostileNpc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGame.Inside_The_Game_System.NPC
+{
+    /// <summary>
+    /// Враждебные боты
+    /// </summary>
+    public class HostileNpc : Npc
+    {
+        /// <summary>
+        /// Бот Враждебный
+        /// </summary>
+        public string? StateHostile = "Враждебный";
+    }
+}
