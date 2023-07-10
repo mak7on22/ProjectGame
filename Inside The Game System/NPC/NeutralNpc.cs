9189a21bbfae647b29b47f511782577c04866662
mak7on22 <mur-mur-0998@mail.ru>
2023-07-11 02:15:59 +0600

    Aded class NeutalNpc

---
+++ Inside The Game System/NPC/NeutralNpc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGame.Inside_The_Game_System.NPC
+{
+    /// <summary>
+    /// Нейтральные боты
+    /// </summary>
+    public class NeutralNpc : Npc
+    {
+        /// <summary>
+        /// Бот Нейтральный
+        /// </summary>
+        public string? StateNeutral = "Нейтральный";
+    }
+}
