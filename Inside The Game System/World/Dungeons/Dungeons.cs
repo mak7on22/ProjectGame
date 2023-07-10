f1e38fe813ec054fcc6c4a230ff70718ee6cbb25
mak7on22 <mur-mur-0998@mail.ru>
2023-07-11 02:14:17 +0600

    Aded class Dungeons

---
+++ Inside The Game System/World/Dungeons/Dungeons.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGame.Inside_The_Game_System.World.Dungeons
+{
+    /// <summary>
+    /// ДАНЖИ
+    /// </summary>
+    public class Dungeons : InsideTheGameSystem
+    {
+        /// <summary>
+        /// Название Данжа
+        /// </summary>
+        public string? NameDungeons {get; set;}
+        /// <summary>
+        /// Lvl Данжа
+        /// </summary>
+        public float LvlDungeons { get; set;}
+    }
+}
