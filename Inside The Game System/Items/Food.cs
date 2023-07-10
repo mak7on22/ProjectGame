889a6e299a535be57fd02c3833c1c22af673d6a9
mak7on22 <mur-mur-0998@mail.ru>
2023-07-11 02:18:20 +0600

    Aded class Food

---
+++ Inside The Game System/Items/Food.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGame.Inside_The_Game_System.Items
+{
+    /// <summary>
+    /// Еда
+    /// </summary>
+    public class Food : Items
+    {
+        /// <summary>
+        /// Срок годности Еды
+        /// </summary>
+        public ulong ExpirationDate { get; set; }
+    }
+}
