cffb4570aef8d08775cd6d70d2f7ff106e229dad
mak7on22 <mur-mur-0998@mail.ru>
2023-07-11 02:18:47 +0600

    Aded class InsideTheGameSystem

---
+++ Inside The Game System/InsideTheGameSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGame.Inside_The_Game_System
+{
+    /// <summary>
+    /// Внутри Игровая Система
+    /// </summary>
+    public class InsideTheGameSystem
+    {
+
+    }
+}
