d44d9d0029c7e6278cba4b41eb09ab38b9d7554b
mak7on22 <mur-mur-0998@mail.ru>
2023-07-11 02:18:10 +0600

    Aded class ItemsWithNoDurability

---
+++ Inside The Game System/Items/ItemsWithNoDurability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGame.Inside_The_Game_System.Items
+{
+    /// <summary>
+    /// Предметы не имеющие прочность
+    /// </summary>
+    public class ItemsWithNoDurability : Items
+    {
+
+    }
+}
