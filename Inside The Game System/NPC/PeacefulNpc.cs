26aca0cf85383af974d509332c1fac43b0903bfe
mak7on22 <mur-mur-0998@mail.ru>
2023-07-11 02:15:37 +0600

    Aded class PeacefulNpc

---
+++ Inside The Game System/NPC/PeacefulNpc.cs
@@ -0,0 +1,22 @@
+﻿using ProjectGame.Inside_The_Game_System;
+using ProjectGame.Inside_The_Game_System.NPC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGame.NPC
+{
+    /// <summary>
+    /// Дружелюбные Боты
+    /// </summary>
+    public class PeacefulNpc : Npc
+    {
+        /// <summary>
+        /// Бот Дружелюбный
+        /// </summary>
+        public string? StatePeaceful = "Дружелюбный";
+
+    }
+}
