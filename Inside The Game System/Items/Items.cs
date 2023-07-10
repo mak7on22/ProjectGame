f15bf1b4bc4fa112513a0cadafd4bd2c1850074f
mak7on22 <mur-mur-0998@mail.ru>
2023-07-11 02:16:44 +0600

    Aded class Items

---
+++ Inside The Game System/Items/Items.cs
@@ -0,0 +1,28 @@
+﻿using ProjectGame.Inside_The_Game_System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGame.Inside_The_Game_System.Items
+{
+    /// <summary>
+    /// Предметы
+    /// </summary>
+    public class Items 
+    {
+        /// <summary>
+        /// НАзвание предмета
+        /// </summary>
+        public string? NameItems { get; set; }
+        /// <summary>
+        /// Левл предмета
+        /// </summary>
+        public float? LvlItems { get; set; }
+        /// <summary>
+        /// Вес предмета
+        /// </summary>
+        public byte Weight { get; set; }
+    }
+}
